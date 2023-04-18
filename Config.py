7eac0a60aa165e3815b8eda5545eca27495a0965
YaDatMe23 <yeahthatme1012@gmail.com>
2023-04-18 07:07:57 -0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,17 +1,16 @@
 import os
-
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
-    AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
-    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
+API_ID = int(os.environ.get("API_ID", "23317761"))
+API_HASH = os.environ.get("API_HASH", "836056bf65b2bc71f06bc2b4dc0b7781")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6298248613:AAGattK9DTIWWHIaNUKMCTHw9F5dvVyPOHM")
+STRING_SESSION = os.environ.get("STRING_SESSION", "1AZWarzoBu4h4hKeCk8hQIS7vjVMeBdxYzdUexrdKpUN00jjsKQamaXPQkI0dLSrs4TKQHaFoycBfNsNrqr5_Bp1JENLP7itJbOgHLCsNruv1QxmfCZqCvyZlOvz87w85FEVxl-98LfOhTvd0LqK6hrFk0uUVo-FjY96j-jVAalCTzhKMIqoJTB1sc28Jls5zzTIjbkVNl7kmQF9CD5qAo72kZDzhv6gUuloX4i3nyoAq6nBGVaeLE8hZDiIBroVtjKEE07JYMC_1SvmU7tKOcsUVaunvWNOShl8Dt6VetUiJ92C0Rg6aP3fDg16zAOMLq9qviLNf_dChbqa9VFV9ak85aKudjtw=")
+MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "My_Yuk_Bot")
+SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")
+CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") 
+START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
+CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "6139072029")) 
+AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) 
+AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) 
